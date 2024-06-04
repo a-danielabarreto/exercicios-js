983632ca63e688db36a57228643e4d85eb6863dd
Daniela <danielabna@gmail.com>
2024-06-04 18:27:57 -0300

    adicionando exercicio15.js

---
+++ exercicio15.js
@@ -0,0 +1,9 @@
+/*
+15 - Crie um script que declare um array de nomes de frutas e utilize um loop for para imprimir cada fruta em uma linha separada.
+*/
+
+const frutas = ["banana", "laranja", "morango", "uva", "maçã", "cajá", "manga", "limão"];
+
+for(i = 0; i < 8; i++) {
+    console.log(frutas[i]);
+}
