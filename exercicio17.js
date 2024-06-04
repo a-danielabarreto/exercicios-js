dad6f9467a249d8f9b549e2553687c9ad8a5328d
Daniela <danielabna@gmail.com>
2024-06-04 18:47:43 -0300

    adicionando exercicio17.js

---
+++ exercicio17.js
@@ -0,0 +1,13 @@
+/*
+17 - Crie um script que declare um objeto chamado livro com as propriedades titulo, autor e anoPublicacao. Inicialize o objeto com valores e imprima todas as propriedades usando console.log.
+*/
+
+let livro = {
+    titulo: "O milagre da manhã",
+    autor: "Hal Elrod",
+    anoPublicacao: 2016 
+}
+
+console.log(`Título: ${livro.titulo}`);
+console.log(`Autor: ${livro.autor}`);
+console.log(`Ano Publicação: ${livro.anoPublicacao}`);
