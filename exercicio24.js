f3ce63fc682267a46007a8ddead64a6d84dad62a
Daniela <danielabna@gmail.com>
2024-06-05 14:10:37 -0300

    adicionando exercicio24.js

---
+++ exercicio24.js
@@ -0,0 +1,9 @@
+/*
+24 - Crie um script que declare um array de strings e utilize a função join() para concatenar todas as strings em uma única string, separadas por vírgulas. Imprima o resultado.
+*/
+
+const linguagens = ["python", "java", "javascript", "go", "php"];
+
+const linguagensConcatenadas = linguagens.join();
+
+console.log(linguagensConcatenadas);
