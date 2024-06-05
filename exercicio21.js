4a56fb9a755abb54f9bb5f2c7d33b88e44173801
Daniela <danielabna@gmail.com>
2024-06-05 13:50:26 -0300

    adicionando exercicio21.js

---
+++ exercicio21.js
@@ -0,0 +1,12 @@
+/*
+21 - Crie um script que declare um array de números e utilize a função filter() para criar um novo array contendo apenas os números pares. Imprima o novo array.
+*/
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//Função de callback
+const numPar = numero => (numero % 2 == 0);
+
+const numerosPares = numeros.filter(numPar);
+
+console.log(numerosPares);
