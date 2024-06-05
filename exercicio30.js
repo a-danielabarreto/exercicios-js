332c6550b62b9b38397cd3c44a023b7bcbbab8ea
Daniela <danielabna@gmail.com>
2024-06-05 15:41:01 -0300

    adicionando exercicio30.js

---
+++ exercicio30.js
@@ -0,0 +1,13 @@
+/*
+30 - Crie um script que declare um objeto chamado carro com as propriedades modelo, ano e cor. Inicialize o objeto com valores e imprima todas as propriedades usando console.log.
+*/
+
+let carro = {
+    modelo: "Onix LTZ",
+    ano: 2019,
+    cor: "Branco"
+};
+
+console.log(`Modelo: ${carro.modelo}`);
+console.log(`Ano: ${carro.ano}`);
+console.log(`Cor: ${carro.cor}`);
