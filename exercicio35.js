e82909af793e2985cdf75bccb83743a64d78f49d
Daniela <danielabna@gmail.com>
2024-06-13 10:14:57 -0300

    adicionando exercicio35.js

---
+++ exercicio35.js
@@ -0,0 +1,9 @@
+/*
+35 - Crie um script que declare uma função que receba um array de números como parâmetro e retorne um novo array onde cada número seja decrementado em 1.
+*/
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const numerosDecrementadosDeUm = numeros.map(numero => numero - 1);
+
+console.log(`Novo array: ${numerosDecrementadosDeUm}`);
